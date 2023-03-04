d4d22f025ed6c9e9ad2442d97c7f1ed2aabc5935
Juan2054 <juanpalleja20@gmail.com>
2023-03-04 15:15:55 -0500

    Funcion de roundRobinFIFO

--- roundRobin.js
+++ roundRobin.js
@@ -1,2 +1,22 @@
 
+function calcularRR(){
+    quantum = 2;
+    procesos = [{name:"P0",rafaga:5},{name:"P1",rafaga:4}];
+    resultado = []
+    for (i = 0; i < procesos.length; i++) {
+        aux = Object.assign({}, procesos[i]);
+        if(procesos[i].rafaga > quantum){
+            aux.rafaga = aux.rafaga - quantum;
+            procesos.push(aux)
+            procesoRealizado = {name:aux.name,rafaga:quantum};
+            resultado.push(procesoRealizado)
+        }
+        else{
+            resultado.push(aux)
+        }
+    } 
+    console.log(resultado);
+    console.log(procesos);
+}
 
+calcularRR();

f2f7c5dd77d28e4607b96260867bbb772c801986
JuanOrozco03 <juanalejandro.orozco@utp.edu.co>
2023-03-04 17:56:43 -0500

    Recibir datos de textarea y mejora del diseno

--- roundRobin.js
+++ roundRobin.js
@@ -1,7 +1,5 @@
-
-function calcularRR(){
-    quantum = 2;
-    procesos = [{name:"P0",rafaga:5},{name:"P1",rafaga:4}];
+function calcularRR(objeto,quantum){
+    procesos = objeto;
     resultado = []
     for (i = 0; i < procesos.length; i++) {
         aux = Object.assign({}, procesos[i]);
@@ -19,4 +17,26 @@ function calcularRR(){
     console.log(procesos);
 }
 
-calcularRR();
+function recibirDatos(){
+    datos = [];
+    objeto = [];
+    quantum = document.querySelector('#quantum-receiver').value;
+    vector = document.querySelector('#data-receiver').value;
+    separador = vector.split(' ');
+    for(let i = 0; i < separador.length; i++){
+        dato = separador[i].split(':');
+        datos.push(dato);
+    }
+    for(let i = 0; i < datos.length; i++){
+        for(let o = 0; o < datos[i].length; o++){
+            datos[i][1] = Number(datos[i][1])
+        }
+    }
+    for(let p = 0; p < datos.length; p++){
+        prueba = {name:datos[p][0],rafaga:datos[p][1]};
+        objeto.push(prueba);
+    }
+    console.log(objeto)
+    calcularRR(objeto,quantum)
+}
+
